a8526edccd0e55236cf8ed801574ef9190597b0a
Josh W <josher19@users.sf.net>
2012-12-27 15:49:17 +0800

    clear TOC list

--- examples/ok/toc.js
+++ examples/ok/toc.js
@@ -1,6 +1,7 @@
 var toc = function (nav, selector){
   nav = nav || document.querySelector('nav ul.toc');
   var i$, ref$, len$, span, txt, li, a, results$ = [];
+  nav.innerHTML = '';
   for (i$ = 0, len$ = (ref$ = document.querySelectorAll(selector || 'code span.function.name,span.meta.function-call')).length; i$ < len$; ++i$) {
     span = ref$[i$];
     txt = span.textContent || span.innerHTML;
